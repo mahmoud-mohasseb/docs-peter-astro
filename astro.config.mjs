409b1357c7e03535a49256d7b340a414b0f663c1
Mac X <ghareb4@gmail.com>
2022-10-06 17:06:33 +0300

    :sparkles: tailwindastro
    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/edge-functions';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/edge-functions";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	output: 'server',
-	adapter: netlify(),
-	site: `http://astro.build`,
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(), // Enable React for the Algolia search component.
+    react(),
+    tailwind(),
+  ],
+  output: "server",
+  adapter: netlify(),
+  site: `http://astro.build`,
 });
